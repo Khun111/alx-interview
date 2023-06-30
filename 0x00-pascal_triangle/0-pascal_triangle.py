26e1204f8983a58e8af60c0b57200186f9a83300
Oluwatobi <amure387@gmail.com>
2023-06-30 20:34:06 +0000

    feat: completed function for oascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+'''Module for Pascal triangle'''
+def pascal_triangle(n):
+    '''Function that returns pascal_triangle'''
+    triangle = []
+    for row in range(n):
+        current_row = []
+        for col in range(row + 1):
+            if col == 0 or col == row:
+                current_row.append(1)
+            else:
+                previous_row = triangle[row - 1]
+                current_row.append(previous_row[col - 1] + previous_row[col])
+        triangle.append(current_row)
+    return triangle
