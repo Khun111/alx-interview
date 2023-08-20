466cb4d07d05907b9ad32cedcae30221a39531bf
Oluwatobi <amure387@gmail.com>
2023-08-20 08:39:22 +0000

    feat: log parsing task done

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from sys import stdin
+from collections import Counter
+
+total_size = 0
+status_dict = Counter()
+line_count = 0
+possible = [200, 301, 400, 401, 403, 404, 405, 500]
+try:
+    for log in stdin:
+        if len(log.split()) == 9:
+            status_code, file_size = log.split()[-2], log.split()[-1]
+            if status_code.isdigit() and int(status_code) in possible:
+                status_dict[status_code] += 1
+                total_size += int(file_size)
+                line_count += 1
+        if line_count % 10 == 0:
+            print("File size: {}".format(total_size))
+            for code, count in sorted(status_dict.items()):
+                print("{}: {}".format(code, count))
+except KeyboardInterrupt:
+    print("File size: {}".format(total_size))
+    for code, count in sorted(status_dict.items()):
+        print("{}: {}".format(code, count))
