c812643a12515b5d599710edd91a85beeac11d4b
Oluwatobi Amure <106190886+Khun111@users.noreply.github.com>
2023-09-07 20:43:54 +0100

    feat: fetched characters in order

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,5 +1,20 @@
 request = require('request')
 
-film = request(`https://swapi.dev/api/films/${process.argv[1]}`)
+function fetchCharactersByIndex(characters, index) {
+    if (index >= characters.length) process.exit(0)
+    request(characters[index], (error, response, body) => {
+        if (error || response.statusCode !== 200) console.log(error)
+        actor = JSON.parse(body);
+        console.log(actor.name)
+        fetchCharactersByIndex(characters, index + 1)
+    })
+}
+request(`https://swapi.dev/api/films/${process.argv[2]}`, (error, response, body) => {
+    if (error || response.statusCode !== 200) console.log(error)
+    film = JSON.parse(body);
+    characters = film.characters
+    fetchCharactersByIndex(characters, 0)
+})
 
-console.log(film)
+
+
