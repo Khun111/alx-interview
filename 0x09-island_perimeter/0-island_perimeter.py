63220eb859563691b7eb38a1bf6d3e08f1591949
Oluwatobi <amure387@gmail.com>
2023-09-27 13:50:02 +0000

    feat: completed island perimeter function

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Module for island perimeter task'''
+
+def island_perimeter(grid):
+    '''Island perimeter function'''
+    perimeter = 0
+    if not grid or len(grid) == 0:
+        return
+    row = len(grid)
+    col = len(grid[0])
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
